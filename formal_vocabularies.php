079d04e6a465f7f4379095c1ba8671171b762c34
jason peak <jpeak5@lsu.edu>
2013-02-24 14:16:29 -0600

    dumps all but what is necessary

--- formal_vocabularies.php
+++ formal_vocabularies.php
@@ -4,52 +4,21 @@
  */
 return $formalVocabularies = array(
     array(
-        'name' => 'PRL1', 
-        'description' => 'PRL Dynamic, Expanding Organic Vocabulary, first iteration', 
-        'namespace_prefix' => 'prl1', 
-        'namespace_uri' => 'http://literati.cct.lsu.edu/prl1', 
+        'name' => 'PRL', 
+        'description' => 'ontology to use until we discover more appropriate standards', 
+        'namespace_prefix' => 'prl', 
+        'namespace_uri' => 'http://literati.cct.lsu.edu/prl', 
         'properties' => array(
             array(
-                'local_part' => 'exemplifiesConcept', 
-                'label' => 'Exemplifies Concept', 
-                'description' => 'This Exemplifies one of the main PRL Concepts (eg Jingoism, American Exceptionalism)'
-            ), 
-            array(
-                'local_part' => 'characterOfTale', 
-                'label' => 'Character Belongs to Tale', 
-                'description' => 'for characters, this links them with their story.'
-            ),
-            array(
-                'local_part' => 'elementOfTale', 
-                'label' => 'Element of Tale', 
-                'description' => "non-character element of a story; Brandreth's Pills, for example"
-            ), 
-            array(
-                'local_part' => 'settingOfTale', 
-                'label' => 'Setting of Tale', 
-                'description' => 'this item appears as a setting of the tale'
-            ), 
-            array(
-                'local_part' => 'referencedHistoricEvent', 
-                'label' => 'Referenced Historic Event', 
-                'description' => 'an historic event referenced directly or by allusion in tale'
-            ), 
-            array(
-                'local_part' => 'eventReportage', 
-                'label' => 'Reportage of Historic Event', 
-                'description' => 'Newspaper article, cartoon, photo, etc that constitutes historical reportage of a significant historic event.'
-            ), 
-            array(
-                'local_part' => 'settingOfEvent', 
-                'label' => 'Setting of historic event', 
-                'description' => 'this points from an event in history to its setting'
-            ),
-            array(
-                'local_part' => 'representativeDepictionOf', 
+                'local_part' => 'isRepresentativeDepictionOf', 
                 'label' => 'Representative Depiction', 
-                'description' => 'This image is representative of some concept, item, tale, event. 
-                    This helps identify imagery to use as a cover image.'
-            ), 
+                'description' => '[image] is representative of [concept | item | tale | event]'
+            ),
+            array(
+                'local_part' => 'isSignificantElementOf', 
+                'label' => 'is Significant Element of', 
+                'description' => '[Event | Place] is Significant Element of [Tale | Letter | Concept]'
+            ),
 
         )
     ), 
